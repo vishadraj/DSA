bf1c4d0940585b71b1d5dc61aa3214a6d87ab74c
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-03-30 22:36:13 +0530

    complete the method findTheTargetInMatrix and create the main method

--- src/SearchIn2DMatrix.java
+++ src/SearchIn2DMatrix.java
@@ -6,13 +6,21 @@
 
 public class SearchIn2DMatrix {
     public static void main(String[] args) {
-
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        System.out.println("findTheTargetInMatrix  = " + findTheTargetInMatrix(matrix, 6));
     }
 
     public static boolean findTheTargetInMatrix(int[][] givenMatrix, int target) {
         int left = 0;
         int right = givenMatrix[0].length - 1;
-
+        while (left < givenMatrix.length && right >= 0) {
+            if (givenMatrix[left][right] == target)
+                return true;
+            else if (givenMatrix[left][right] > target)
+                right--;
+            else if (givenMatrix[left][right] < target)
+                left++;
+        }
         return false;
     }
 }
